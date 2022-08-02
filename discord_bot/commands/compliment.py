256a1e3479a8903aac2c1f3df36d04b630cd33cb
Rafhael Payão <rafaeltmbr@gmail.com>
2022-08-01 23:26:38 -0300

    fix: mispelled compliment command

--- discord_bot/commands/compliment.py
+++ discord_bot/commands/compliment.py
@@ -5,9 +5,9 @@ import random
 from ..bot import bot
 from ..util.get_file_lines import get_file_lines
 
-phrases = get_file_lines('discord_bot/content/phrases/compliement.txt')
+phrases = get_file_lines('discord_bot/content/phrases/compliment.txt')
 
-@bot.command(name='elogiar', aliases=['compliement'])
-async def compliement(ctx: commands.Context, who):
+@bot.command(name='elogiar', aliases=['compliment'])
+async def compliment(ctx: commands.Context, who):
     phrase = phrases[random.randrange(len(phrases))]
     await ctx.send(f'{who} {phrase}')
