d2b738dceaf72f0e1369fd43e279165a48829b46
Rafhael Payão <rafaeltmbr@gmail.com>
2022-08-01 23:18:05 -0300

    fix: added new role to the mother command

--- discord_bot/commands/mother.py
+++ discord_bot/commands/mother.py
@@ -6,14 +6,12 @@ from ..util.get_file_lines import get_file_lines
 
 phrases = get_file_lines('discord_bot/content/phrases/mother.txt')
 
-AUTHORIZED_ROLES = ['adm', 'adms', 'admin', 'admins', "Pk's Academy"]
+AUTHORIZED_ROLES = ['adm', 'adms', 'admin', 'admins', "pk's academy"]
 NOT_AUTHORIZED_MESSAGE = 'com grandes poderes vem grandes responsabilidades.'
 
 @bot.command(name='mae', aliases=['mãe', 'mother'])
 async def mother(ctx: commands.Context, who):
     author_roles = [role.name.lower() for role in ctx.author.roles]
-
-    print(ctx.author.roles)
 
     if set(AUTHORIZED_ROLES) & set(author_roles):
         phrase = phrases[random.randrange(len(phrases))]
